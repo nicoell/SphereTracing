7dd8a90566514b67e578b0c32a3efe523f2dc509
Nico Ell <nicoell@uni-koblenz.de>
2018-04-18 13:46:46 +0200

    Add initial project setup

---
+++ Assets/Scripts/Sphere Tracing/Shader/RenderShader.shader
@@ -0,0 +1,63 @@
+﻿Shader "SphereTracing/RenderShader"
+{
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+		Tags{ "Queue" = "Overlay" }
+
+		Pass
+		{
+			CGPROGRAM
+			
+			#pragma target 5.0
+			#pragma only_renderers d3d11
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			Texture2DArray SphereTracingArray;
+			SamplerState samplerSphereTracingArray;
+			
+			uniform int ArrayIndex;
+
+            // Primitive Assembler Out
+			struct PAOut
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+            // Vertex Shader Out
+			struct VSOut
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+			
+			struct PSOut 
+			{
+			    float4 color : SV_Target;
+			};
+
+			VSOut vert (PAOut IN)
+			{
+				VSOut OUT;
+				OUT.vertex = float4(IN.vertex.xy*2,0,1);
+				//OUT.vertex = UnityObjectToClipPos(IN.vertex);
+				OUT.uv = IN.uv;
+				return OUT;
+			}
+			
+			PSOut frag (VSOut IN)
+			{
+			    PSOut OUT;
+			    OUT.color = SphereTracingArray.Sample(samplerSphereTracingArray, float3(IN.uv.xy, ArrayIndex));
+
+				return OUT;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Scripts/Sphere Tracing/SphereTracingManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class SphereTracingManager : MonoBehaviour
+{
+	private int _sphereTracingKernel;
+	private MaterialPropertyBlock _targetRendererProperties;
+	private RenderTexture _targetRenderTextureArray;
+	private Resolution _targetResolution;
+	private int _threadGroupX, _threadGroupY, _threadGroupZ;
+	public Mesh QuadMesh;
+
+	[Range(0, 2)]
+	public int RenderMode;
+	public ComputeShader SphereTracingShader;
+	public Material TargetRendererMaterial;
+
+	[Range(-50, 50)]
+	public float CameraDistance = 5f;
+	[Range(0.1f, 3f)]
+	public float FOV = 1f;
+
+
+	// Use this for initialization
+	private void Start()
+	{
+		_targetRendererProperties = new MaterialPropertyBlock();
+
+		_targetResolution = Screen.currentResolution;
+
+		_targetRenderTextureArray = new RenderTexture(_targetResolution.width, _targetResolution.height, 0,
+			RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear)
+		{
+			dimension = TextureDimension.Tex2DArray,
+			volumeDepth = 3,
+			enableRandomWrite = true
+		};
+		_targetRenderTextureArray.Create();
+
+		_sphereTracingKernel = SphereTracingShader.FindKernel("CSMain");
+
+		uint threadGroupX, threadGroupY, threadGroupZ;
+		SphereTracingShader.GetKernelThreadGroupSizes(_sphereTracingKernel, out threadGroupX,
+			out threadGroupY, out threadGroupZ);
+
+		_threadGroupX = (int) (_targetResolution.width / threadGroupX);
+		_threadGroupY = (int) (_targetResolution.height / threadGroupY);
+		_threadGroupZ = 1;
+
+		_targetRendererProperties.SetTexture("SphereTracingArray", _targetRenderTextureArray);
+		Shader.SetGlobalTexture("SphereTracingArray", _targetRenderTextureArray);
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		TargetRendererMaterial.SetInt("ArrayIndex", RenderMode);
+		
+		
+		Shader.SetGlobalInt("ArrayIndex", RenderMode);
+
+		RunSphereTracing();
+		Graphics.DrawMesh(QuadMesh, Matrix4x4.identity, TargetRendererMaterial, 0, Camera.current, 0,
+			_targetRendererProperties);
+	}
+
+	private void RunSphereTracing()
+	{
+		SphereTracingShader.SetFloat("CameraDistance", CameraDistance);
+		SphereTracingShader.SetFloat("FOV", FOV);
+		SphereTracingShader.SetFloat("Width", _targetResolution.width);
+		SphereTracingShader.SetFloat("Height", _targetResolution.height);
+		SphereTracingShader.SetTexture(_sphereTracingKernel, "SphereTracingArray", _targetRenderTextureArray);
+		SphereTracingShader.Dispatch(_sphereTracingKernel, _threadGroupX, _threadGroupY, _threadGroupZ);
+	}
+}
