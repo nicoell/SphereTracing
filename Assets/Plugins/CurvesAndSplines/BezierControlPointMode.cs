56e663fa6bac97b18114b497d85c4219dc028672
Nico Ell <nicoell@uni-koblenz.de>
2018-06-19 15:43:33 +0200

    Add simple plugin for animation bezierspline

---
+++ Assets/Plugins/CurvesAndSplines/BezierControlPointMode.cs
@@ -0,0 +1,9 @@
+﻿namespace CurvesAndSplines
+{
+	public enum BezierControlPointMode
+	{
+		Free,
+		Aligned,
+		Mirrored
+	}
+}
---
+++ Assets/Plugins/CurvesAndSplines/Line.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CurvesAndSplines
+{
+	public class Line : MonoBehaviour
+	{
+
+		public Vector3 p0, p1;
+	}
+}
