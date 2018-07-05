761bb257903031b8fc5a1d3f6565fdeb5db2465d
Nico Ell <nicoell@uni-koblenz.de>
2018-07-05 14:56:46 +0200

    Fix build error by only using hidegizmotool in editor

--- Assets/Scripts/Utils/HideToolGizmos.cs
+++ Assets/Scripts/Utils/HideToolGizmos.cs
@@ -6,8 +6,10 @@ namespace Utils
 	[ExecuteInEditMode]
 	public class HideToolGizmos : MonoBehaviour
 	{
+		#if UNITY_EDITOR
 		private void OnEnable() { Tools.hidden = true; }
 
 		private void OnDisable() { Tools.hidden = false; }
+		#endif
 	}
 }
